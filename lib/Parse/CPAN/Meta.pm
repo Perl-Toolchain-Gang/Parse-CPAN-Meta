b8c36c2794933af620faa72e1e0526bffcf6173b
David Golden <xdg@xdg.me>
2016-06-28 16:59:45 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/Parse/CPAN/Meta.pm
+++ lib/Parse/CPAN/Meta.pm
@@ -3,7 +3,7 @@ use strict;
 package Parse::CPAN::Meta;
 # ABSTRACT: Parse META.yml and META.json CPAN metadata files
 
-our $VERSION = '1.4421';
+our $VERSION = '1.4422';
 
 use Exporter;
 use Carp 'croak';
